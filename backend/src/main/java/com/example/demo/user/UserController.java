e9b66d61f27f7590c4ce3bbe95405b5194d3ccc4
Your Name <you@example.com>
2025-11-11 13:18:20 +0530

    feat: Add WebFlux dependency and implement asynchronous POST request in UserController

--- backend/src/main/java/com/example/demo/user/UserController.java
+++ backend/src/main/java/com/example/demo/user/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo.user;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.reactive.function.client.WebClient;
 
-
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api/users/")
@@ -18,9 +20,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserController {
 
 	private final UserRepository repository;
+	private final WebClient webClient;
 	
-	public UserController(UserRepository repository) {
+	public UserController(UserRepository repository, WebClient webClient) {
 		this.repository = repository;
+		this.webClient = webClient;
 	}
 	
 	@GetMapping("list")
@@ -49,4 +53,15 @@ public class UserController {
 		}
 	}
 	
+	public Mono<Object> sendPostRequestAsync(Object requestBody) {
+	        String url = "http://localhost:8081/api/target-endpoint";
+
+	        return webClient.post()
+	            .uri(url)
+	            .contentType(MediaType.APPLICATION_JSON)
+	            .header("Autharization", "Bearer xyz")
+	            .body(Mono.just(requestBody), Object.class)
+	            .retrieve()
+	            .bodyToMono(Object.class);
+	    }
 }
