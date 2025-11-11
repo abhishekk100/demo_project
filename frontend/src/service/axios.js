953073ccf6c3a96b12bcb5fe82ed19509d1bae58
Your Name <you@example.com>
2025-11-11 11:39:48 +0530

    feat: Update project configuration and add database scripts - Enhance .gitignore with deployment guides - Modify application.properties for database credentials - Clean up package-lock.json by removing unnecessary dependencies - Update axios.js to use environment variable for API URL - Remove legacy plugin from vite.config.js - Add MySQL dump scripts for demo_db - Introduce environment configuration files for development and production

--- frontend/src/service/axios.js
+++ frontend/src/service/axios.js
@@ -1,8 +1,9 @@
 // src/axios.js
 import axios from "axios";
+// console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
